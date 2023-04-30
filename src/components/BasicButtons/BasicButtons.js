4db6062b153726102a9207f883bc998b9175dc54
Diego de Oliveira Santos <doliveirasantos98@gmail.com>
2023-04-30 01:30:42 -0300

    feat: display now show more number and texts

--- src/components/BasicButtons/BasicButtons.js
+++ src/components/BasicButtons/BasicButtons.js
@@ -6,15 +6,21 @@ import { GridView } from "./style"
 
 const BasicButtons = ({ display, setDisplay, result, setResult }) => {
     const BtnText = ['7', '8', '9', 'AC', 'DEL', '4', '5', '6', 'x', '÷', '1', '2', '3', '+', '-', '0', '.', 'EXP', 'Ans', '=']
-
-    const handlePress = () => {
+    
+    const handlePress = (text) => {
+        const arr = []
         
+        if(text == '+'){
+
+        }
+        arr.push(...display, text)
+        setDisplay(arr)
     }
 
     return (
         <GridView>
             {BtnText.map((text, index) => (
-                <Button key={ index } onPress={ handlePress }>{ text }</Button>
+                <Button key={ index } onPress={ () => handlePress(text) }>{ text }</Button>
             ))}
         </GridView>
     )
